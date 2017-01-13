fb135ba32c80422bc0fb6856a0f15aade0d0430c
Fredrik Dahlberg <fd@abc.se>
2017-01-13 17:06:09 +0100

    Started adding models

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class ItemCategory(models.Model):
+	name = models.CharField(max_length=50)
+
+class ItemType(models.Model):
+	name = models.CharField(max_length=50)
+	category = models.ForeignKey(ItemCategory)
+
+class Item(models.Model):
+	item_type = models.ForeignKey(ItemType)
+	name = models.CharField(max_length=50)
